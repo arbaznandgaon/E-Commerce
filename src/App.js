1e5728f816a13f9e6c7733d21d583e0503a35ae3
Arbaz Nandgaon <arbaznandgaon@gmail.com>
2023-04-09 20:27:03 +0530

    001

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import { BrowserRouter as Router, Route,Link, Routes } from "react-router-dom";
-import Header from './Containers/Header';
-import ProductLsting from './Containers/ProductLsting';
-import ProductDetails from './Containers/ProductDetails';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Header from "./Containers/Header";
+import ProductLsting from "./Containers/ProductLsting";
+import ProductDetails from "./Containers/ProductDetails";
 
 function App() {
   return (
     <div className="">
-<Router>
-<Header/> 
-<Routes>
-<Route eact path="/" element={<ProductLsting />}></Route>
-          <Route path="/product/:productId" element={<ProductDetails/>} />
-         
-<Route>404 Not Found!</Route>
-</Routes>
-</Router>
- 
-  </div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route eact path="/" element={<ProductLsting />}></Route>
+          <Route path="/product/:productId" element={<ProductDetails />} />
 
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
